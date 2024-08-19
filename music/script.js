4371ca925e8f930568127a7bb25312357e699dc2
Nhat Hao <tranlenhathao2000@gmail.com>
2024-08-19 23:33:05 +0700

    add sorting, switch songs automatically

--- music/script.js
+++ music/script.js
@@ -9,3 +9,32 @@ document.addEventListener('DOMContentLoaded', function() {
         audio.play();
     });
 });
+
+var select = document.getElementById("playlist");
+  var options = select.getElementsByTagName("option");
+  
+  var sortedOptions = Array.from(options).sort((a, b) => a.text.localeCompare(b.text));
+  
+  for (var i = 0; i < sortedOptions.length; i++) {
+    select.appendChild(sortedOptions[i]);
+  }
+
+document.getElementById('audio').volume = 0.5; // Set volume to 50%
+
+	const playlist = document.getElementById('playlist');
+    const audio = document.getElementById('audio');
+    
+    playlist.addEventListener('change', function() {
+        const selectedOption = playlist.value;
+        audio.src = `${selectedOption}`;
+        audio.play();
+    });
+    
+    audio.addEventListener('ended', function() {
+		const currentIndex = playlist.selectedIndex;
+		const nextIndex = currentIndex < playlist.options.length - 1 ? currentIndex + 1 : 0;
+		playlist.selectedIndex = nextIndex;
+		const selectedOption = playlist.value;
+		audio.src = `${selectedOption}`;
+		audio.play();
+    });
