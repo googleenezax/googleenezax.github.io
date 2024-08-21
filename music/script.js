bcaada4c8f9f00350ef7a8b599c3a8e0c45cc373
Nhat Hao <tranlenhathao2000@gmail.com>
2024-08-21 21:18:46 +0700

    add 'update push news'

--- music/script.js
+++ music/script.js
@@ -74,3 +74,24 @@ document.addEventListener('click', function(event) {
     window.history.replaceState({}, '', realURL);
   }
 });
+
+function openPopup() {
+        document.getElementById("popup").style.display = "block";
+        document.getElementById("overlay").style.display = "block";
+
+        // Đọc nội dung từ file txt
+    fetch('update.txt')
+      .then(response => response.text())
+      .then(data => {
+        // Hiển thị nội dung trong thẻ div
+        document.getElementById('eventContent').textContent = data;
+      })
+      .catch(error => {
+        console.error('Lỗi khi đọc file:', error);
+      });
+    }
+
+    function closePopup() {
+        document.getElementById("popup").style.display = "none";
+        document.getElementById("overlay").style.display = "none";
+    }
