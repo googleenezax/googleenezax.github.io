6086e7ab86fb7be80e9864c0be35abef5ce50dab
Nhat Hao <tranlenhathao2000@gmail.com>
2024-09-08 18:25:27 +0700

    test

--- music/test.js
+++ music/test.js
@@ -1,18 +1,35 @@
-const fileUrl = 'E:\Github\googleenezax.github.io\files\listMS.xlsx';
+<!DOCTYPE html>
+<html lang="en">
 
-fetch(fileUrl)
-    .then(response => response.arrayBuffer())
-    .then(data => {
-        const workbook = XLSX.read(data, { type: 'array' });
-        const sheetName = 'sheet1'; // Tên của sheet cần truy cập
-        const sheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(sheet);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video Player</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+        }
 
-        jsonData.forEach(row => {
-            const option = document.createElement('option');
-            option.text = row.B; // Tên nhạc từ cột B
-            option.value = row.E; // Đường dẫn từ cột E
-            document.getElementById('musicOptions').appendChild(option);
-        });
-    })
-    .catch(error => console.error('Error fetching the Excel file:', error));
+        video {
+            max-width: 100%;
+            height: auto;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+
+<body>
+    <video controls>
+        <source src="../files/videos/test.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</body>
+
+</html>
